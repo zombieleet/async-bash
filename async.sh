3565a396613d653e7c03e72ed6cf343d86a9bb73
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-02-26 10:17:52 +0100

    add support for execution group of functions is series

--- async.sh
+++ async.sh
@@ -16,10 +16,14 @@ declare -i JOBS=1;
 #source ./functions.sh;
 
 setTimeout() {
+    
     local command="$1"
     local after="$2"
+
+    read -d " " comm <<<"${command}"
+
+    declare -F $comm &>/dev/null
     
-    declare -F $command &>/dev/null
     local status=$?
 
     { (( status != 0 )) || [[ -z "$command" ]] ; } && {
@@ -34,10 +38,10 @@ setTimeout() {
 	return 1;
     }
 
-    (
+    {
 	sleep ${after}
 	$command
-    ) &
+    } &
     
     JOB_IDS+=( "${JOBS} ${command}" )
     
@@ -193,15 +197,18 @@ function async() {
     unset __temp ;  unset _c
     
     {
-	local __result=$($commandToExec)
+
+	__result=$($commandToExec)
 	
 	status=$?
 	
 	(( status == 0 )) && {
 	    $resolve "${__result}"
+	    
 	} || {
 	    $reject "${status}"
 	}
+	unset __result
     } &
 
 
@@ -215,4 +222,156 @@ function async() {
 
     : $(( JOBS++ ))
     
+}
+
+
+series() {
+
+    #local funcArray="${@:1:$(( ${#@} - 1 ))}"
+
+    local mainFunc="${1}"
+    local funcArray="${2}"
+    local finalFunc="${3}"
+
+    local totalArgs=${#@}
+
+    (( totalArgs < 3 )) && {
+	printf "%s\n" "Insufficient number of argument"
+	return 1;
+    }
+
+    read -d " " __cmd <<<"${mainFunc}"
+
+    local _isDef=$(type -t ${__cmd})
+    
+    
+     [[ -z $_isDef ]] && {
+	printf "%s\n" "${__cmd} is not of type { function , alias , builtin or file }"
+	return 1;
+    }
+
+     [[ "$(type -t $finalFunc)" != "function" ]] && {
+	 printf "%s\n" "${finalFunc} is not of type { function }"
+	 return 1;
+     }
+    
+    for __arr in ${funcArray};do
+
+	local __isfunc=$(type -t ${__arr})
+
+	[[ $__isfunc != "function" ]] && {
+	    
+	    printf "%s\n" "${__arr} is not of type { function }"
+	    return 1;
+	    
+	}
+	
+	declare __fArray+=( ${__arr} )
+    done
+
+    unset __arr
+
+    {
+	__result=$($mainFunc)
+
+	status=$?
+	
+	(( status != 0 )) && {
+	    $finalFunc "" "${__result}"
+	    return $?;
+	}
+    
+	local _t=0
+	
+	for __async in "${__fArray[@]}";do
+	    
+	    __result=$(${__async} "${__result}")
+	    
+	    status=$?
+	    
+	    (( status != 0 )) && {
+		$finalFunc "" "${__result}"
+		
+		# _t has no use here, since we will be returning from this function
+		#   it was only use for clarity
+		
+		_t=1
+		return $?
+	    }
+	    
+	done
+	
+	(( _t == 0 )) && {
+	    $finalFunc "${__result}" ""
+	}
+    } &
+	
+}
+
+a=( victory favour johnson )
+
+mainfunc() {
+    while read line;do
+	if [[ "$line" =~ "root" ]];then
+	    echo $line
+	    return 0;
+	else
+	    echo $line
+	    return 0;
+	fi
+    done </etc/passwd
+}
+
+victory() {
+    local line="$1"
+    if [[ $line =~ "bash" ]];then
+	echo "${line##*:}"
+	return 0;
+    fi
+    
+    return 0;
+}
+favour() {
+    
+    local exists="$1"
+    
+    {
+	[[ -e "$exists" ]]  && [[ -f "$exists" ]]
+	    
+    } && {
+	echo $exists
+    }
+
+    return 0;
+}
+johnson() {
+    local file=$(file "$1")
+    if [[ $file =~ "ELF" ]];then
+	echo $file
+    fi
+    
+    return 0;
+}
+
+final() {
+    echo "$1"
+}
+
+series "mainfunc" "${a[*]}" "final"
+
+resolve() {
+    echo "suc"
+}
+
+reject() {
+    echo "$1"
+}
+
+
+get() {
+    curl -s "$1"
+}
+
+getName() {
+    echo "excess"
 }
