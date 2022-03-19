0c88ebb90d3705c68fd869e0af066e59723ef9b4
Victory Osikwemhe <zombieleetnca@gmail.com>
2022-03-19 18:51:14 +0100

    chore: linting

--- async.bash
+++ async.bash
@@ -16,7 +16,7 @@ declare -i JOBS=1;
 #source ./functions.sh;
 
 setTimeout() {
-    
+
     local command="$1"
     local after="$2"
 
@@ -27,29 +27,29 @@ setTimeout() {
     local _isDef=$(type -t ${comm})
 
     if [[ -z "${_isDef}" ]];then
-			printf "%s\n" "\"${command}\" is not of type { function, command} "
-	
-			return 1;
+	printf "%s\n" "\"${command}\" is not of type { function, command} "
+
+	return 1;
     fi
-    
-    
+
+
     [[ ! $after =~ ^[[:digit:]]+$ ]] && {
-			printf "%s\n" "require an integer as the second argument but got \"$after\" "
-	
-			return 1;
+	printf "%s\n" "require an integer as the second argument but got \"$after\" "
+
+	return 1;
     }
 
     {
-			sleep ${after}
-			$command
+	sleep ${after}
+	$command
     } &
-    
-    JOB_IDS+=( "${JOBS} ${command}" )
-    
 
-    
+    JOB_IDS+=( "${JOBS} ${command}" )
+
+
+
     read -d " " -a __kunk__ <<< "${JOB_IDS[$(( ${#JOB_IDS[@]} - 1))]}"
-    
+
     echo ${__kunk__}
 
     : $(( JOBS++ ))
@@ -57,37 +57,37 @@ setTimeout() {
 }
 
 setInterval() {
-    
+
     local command="$1"
     local after="$2"
-    
+
     read -d " " comm <<<"${command}"
-    
 
-    
+
+
     local _isDef=$(type -t ${comm})
-    
+
     if [[ -z "${_isDef}" ]];then
-			printf "%s\n" "\"${command}\" is not of type { function, command} "
+	printf "%s\n" "\"${command}\" is not of type { function, command} "
 
-			return 1;
+	return 1;
     fi
-    
-    
-    [[ ! $after =~ ^[[:digit:]]+$ ]] && {
-			printf "%s\n" "require an integer as the second argument but got \"$after\" "
 
-			return 1;
+
+    [[ ! $after =~ ^[[:digit:]]+$ ]] && {
+	printf "%s\n" "require an integer as the second argument but got \"$after\" "
+
+	return 1;
     }
 
     {
-			while sleep ${after};do
-					$command
-			done
+	while sleep ${after};do
+	    $command
+	done
     } &
-    
+
     JOB_IDS+=( "${JOBS} ${command}" )
-    
+
     read -d " " -a __kunk__ <<< "${JOB_IDS[$(( ${#JOB_IDS[@]} - 1))]}"
 
     echo ${__kunk__}
@@ -96,20 +96,20 @@ setInterval() {
 }
 
 killJob() {
-    
+
     local jobToKill="$1"
     local signal="$2"
-    
+
     signal=${signal^^}
-    
+
     [[ ! $jobToKill =~ ^[[:digit:]]+$ ]] && {
 
-			printf "%s\n" "\"$jobToKill\" should be an integer ";
+	printf "%s\n" "\"$jobToKill\" should be an integer ";
 
-			return 1;
+	return 1;
     }
-    
-    
+
+
     {
 	[[ -z "$signal" ]]  && {
 	    signal="SIGTERM"
@@ -119,114 +119,114 @@ killJob() {
 	local __al__signals=$(kill -l);
 	local isSig=0;
 	for sig in ${__al__signals};do
-	    
-	  [[ ! $sig =~ ^[[:digit:]]+\)$ ]] && {
-				[[ $signal == $sig ]] && {
-						isSig=1;
-						break;
-				}
-	  }
+
+	    [[ ! $sig =~ ^[[:digit:]]+\)$ ]] && {
+		[[ $signal == $sig ]] && {
+		    isSig=1;
+		    break;
+		}
+	    }
 	done
-	
+
 	(( isSig != 1 )) && {
 	    signal="SIGTERM"
 	}
-	
+
     }
-    
-    
+
+
 
     for job in ${JOB_IDS[@]};do
-	
-			# increment job to 1 since array index starts from 0
-			read -d " " -a __kunk__ <<< "${JOB_IDS[$job]}"
 
-			(( __kunk__ == jobToKill )) && {
-	    
+	# increment job to 1 since array index starts from 0
+	read -d " " -a __kunk__ <<< "${JOB_IDS[$job]}"
+
+	(( __kunk__ == jobToKill )) && {
+
 
 	    read -d " " -a __kunk__ <<< "${JOB_IDS[$job]}"
-	    
+
 	    kill -${signal} %${__kunk__}
-	    
+
 	    local status=$?
-	    
+
 	    (( status != 0 )) && {
-		
 
-				printf "cannot kill %s %d\n" "${JOB_IDS[$job]}" "${__kunk__}"
 
-				return 1;
+		printf "cannot kill %s %d\n" "${JOB_IDS[$job]}" "${__kunk__}"
+
+		return 1;
 	    }
 
-	    printf "%d killed with %s\n" "${__kunk__}" "${signal}" 
-	    
+	    printf "%d killed with %s\n" "${__kunk__}" "${signal}"
+
 	    return 0;
 	}
-	
-    done    
+
+    done
 }
 
 async() {
-    
+
     local commandToExec="$1"
     local resolve="$2"
     local reject="$3"
 
     [[ -z "$commandToExec" ]] || [[ -z "$reject" ]] || [[ -z "$resolve" ]] && {
-			printf "%s\n" "Insufficient number of arguments";
-			return 1;
+	printf "%s\n" "Insufficient number of arguments";
+	return 1;
     }
 
-    
-    
+
+
     local __temp=( "$commandToExec" "$reject" "$resolve" )
-    
-    
+
+
     for _c in "${__temp[@]}";do
 
 
-			read -d " " comm <<<"${_c}"
+	read -d " " comm <<<"${_c}"
 
-			type "${comm}" &>/dev/null
-	
+	type "${comm}" &>/dev/null
+
     	local status=$?
-	
+
     	(( status != 0 )) && {
     	    printf "\"%s\" is neither a function nor a recognized command\n" "${_c}";
-	    		unset _c
-	   		 return 1;
+	    unset _c
+	    return 1;
     	}
-	
+
     done
-    
+
     unset __temp ;  unset _c
-    
+
     {
 
-			__result=$($commandToExec)
+	__result=$($commandToExec)
 
-			status=$?
+	status=$?
 
-			(( status == 0 )) && {
-					$resolve "${__result}"
+	(( status == 0 )) && {
+	    $resolve "${__result}"
 
-			} || {
-					$reject "${status}"
-			}
-			unset __result
+	} || {
+	    $reject "${status}"
+	}
+	unset __result
     } &
 
 
-    
+
     JOB_IDS+=( "${JOBS} ${command}" )
-    
+
     read -d " " -a __kunk__ <<< "${JOB_IDS[$(( ${#JOB_IDS[@]} - 1))]}"
-    
+
     echo ${__kunk__}
-    
+
 
     : $(( JOBS++ ))
-    
+
 }
 
 
@@ -241,82 +241,82 @@ parallel() {
     local totalArgs=${#@}
 
     (( totalArgs < 3 )) && {
-			printf "%s\n" "Insufficient number of argument"
-			return 1;
+	printf "%s\n" "Insufficient number of argument"
+	return 1;
     }
 
     read -d " " __cmd <<<"${mainFunc}"
 
     local _isDef=$(type -t ${__cmd})
-    
-    
-     [[ -z $_isDef ]] && {
-			printf "%s\n" "${__cmd} is not of type { function , alias , builtin or file }"
-			return 1;
+
+
+    [[ -z $_isDef ]] && {
+	printf "%s\n" "${__cmd} is not of type { function , alias , builtin or file }"
+	return 1;
     }
 
-     [[ "$(type -t $finalFunc)" != "function" ]] && {
-			 printf "%s\n" "${finalFunc} is not of type { function }"
-			 return 1;
-     }
-    
+    [[ "$(type -t $finalFunc)" != "function" ]] && {
+	printf "%s\n" "${finalFunc} is not of type { function }"
+	return 1;
+    }
+
     for __arr in ${funcArray};do
 
-			local __isfunc=$(type -t ${__arr})
+	local __isfunc=$(type -t ${__arr})
 
-			[[ $__isfunc != "function" ]] && {
+	[[ $__isfunc != "function" ]] && {
 
-					printf "%s\n" "${__arr} is not of type { function }"
-					return 1;
+	    printf "%s\n" "${__arr} is not of type { function }"
+	    return 1;
 
-			}
+	}
 
-			declare __fArray+=( ${__arr} )
+	declare __fArray+=( ${__arr} )
     done
 
     unset __arr
 
     {
-			__result=$($mainFunc)
+	__result=$($mainFunc)
 
-			status=$?
+	status=$?
 
-			(( status != 0 )) && {
-					$finalFunc "" "${__result}"
-					return $?;
-			}
-    
-			local _t=0
+	(( status != 0 )) && {
+	    $finalFunc "" "${__result}"
+	    return $?;
+	}
 
-			for __async in "${__fArray[@]}";do
+	local _t=0
 
-					__result=$(${__async} "${__result}")
+	for __async in "${__fArray[@]}";do
 
-					status=$?
+	    __result=$(${__async} "${__result}")
 
-					(( status != 0 )) && {
-						$finalFunc "" "${__result}"
+	    status=$?
 
-						# _t has no use here, since we will be returning from this function
-						#   it was only use for clarity
+	    (( status != 0 )) && {
+		$finalFunc "" "${__result}"
 
-						_t=1
-						return $?
-				}
-	    
-			done
-	
-			(( _t == 0 )) && {
-					$finalFunc "${__result}" ""
-			}
-   } &
-    
+		# _t has no use here, since we will be returning from this function
+		#   it was only use for clarity
+
+		_t=1
+		return $?
+	    }
+
+	done
+
+	(( _t == 0 )) && {
+	    $finalFunc "${__result}" ""
+	}
+    } &
+
     JOB_IDS+=( "${JOBS} ${command}" )
-    
+
     read -d " " -a __kunk__ <<< "${JOB_IDS[$(( ${#JOB_IDS[@]} - 1))]}"
-    
+
     echo ${__kunk__}
-    
-    
+
+
     : $(( JOBS++ ))
 }
